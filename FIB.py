a0d3da88a381c6c086dd635ba1b529e17c739b18
yintai <diane.yintai.wang@gmail.com>
2022-07-23 19:10:02 +0800

    use cache to speed up

--- FIB.py
+++ FIB.py
@@ -1,16 +1,28 @@
 #Rabbits and Recurrence Relations
 #https://rosalind.info/problems/fib/
 
+#ref: https://www.youtube.com/watch?v=Qk0zUZW-U_M
+
+cache = {}
 
 def wabbits(n, k):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return wabbits(n-1, k) + wabbits(n-2, k) * k
+    # If we have cached the value, then return it
+    if n in cache:
+        return cache[n]
 
-with open('rosalind_fib.txt') as f:
-    n, k = f.readline().strip().split(' ')
+    # Compute the Nth term    
+    if n == 0:
+        value = 0
+    elif n == 1:
+        value = 1
+    else:
+        value = wabbits(n-1, k) + wabbits(n-2, k) * k
     
+    # Cache the value and return it
+    cache[n] = value
+    return value
+
+with open('rosalind_fib.txt', 'r') as f:
+    n, k = f.readline().strip().split(' ')
+
 print(wabbits(int(n), int(k)))
